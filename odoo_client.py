84c590fac474e10ec2def9589b48937bb469055c
pratzs <pratzs@live.com>
2025-12-08 16:31:08 +1300

    Update odoo_client.py

--- odoo_client.py
+++ odoo_client.py
@@ -7,6 +7,7 @@ class OdooClient:
         self.db = db
         self.username = username
         self.password = password
+        # Fix for some free hosting SSL contexts
         self.context = ssl._create_unverified_context()
         
         self.common = xmlrpc.client.ServerProxy(f'{self.url}/xmlrpc/2/common', context=self.context)
@@ -25,7 +26,6 @@ class OdooClient:
     def search_product_by_sku(self, sku, company_id=None):
         """Finds product ID by SKU, filtering by Company if provided (Fixed Syntax)"""
         if company_id:
-            # POLISH NOTATION FIX: ['&', A, '|', B, C]  ==  A AND (B OR C)
             domain = [
                 '&', 
                 ['default_code', '=', sku], 
@@ -61,9 +61,9 @@ class OdooClient:
         """Creates a new Service product for shipping charges"""
         vals = {
             'name': name,
-            'type': 'service',  # Service type so it doesn't track stock
+            'type': 'service',  
             'invoice_policy': 'order',
-            'list_price': 0.0,  # Price determined by order line
+            'list_price': 0.0,
             'sale_ok': True,
             'purchase_ok': False,
         }
@@ -76,134 +76,9 @@ class OdooClient:
     def get_changed_products(self, time_limit_str, company_id=None):
         """Finds IDs of products changed recently, filtered by Company."""
         if company_id:
-            # Get products modified recently AND (belong to Company OR Shared)
             domain = [
                 '&',
-                '&', # Nested AND for the write_date/type
-                ['write_date', '>', time_limit_str], 
-                ['type', '=', 'product'],
-                '|',
-                ['company_id', '=', int(company_id)],
-                ['company_id', '=', False]
-            ]
-        else:
-            domain = [('write_date', '>', time_limit_str), ('type', '=', 'product')]
-
-        return self.models.execute_kw(self.db, self.uid, self.password,
-            'product.product', 'search', [domain])
-
-    def get_companies(self):
-        return self.models.execute_kw(self.db, self.uid, self.password,
-            'res.company', 'search_read', [[]], 
-            {'fields': ['id', 'name']})
-
-    def get_locations(self, company_id=None):
-        if not company_id: return []
-        domain = [['usage', '=', 'internal'], ['company_id', '=', int(company_id)]]
-        return self.models.execute_kw(self.db, self.uid, self.password,
-            'stock.location', 'search_read', [domain], 
-            {'fields': ['id', 'complete_name', 'company_id']})
-
-    def get_total_qty_for_locations(self, product_id, location_ids, field_name='qty_available'):
-        total_qty = 0
-        for loc_id in location_ids:
-            context = {'location': loc_id}
-            data = self.models.execute_kw(self.db, self.uid, self.password,
-                'product.product', 'read', [product_id],
-                {'fields': [field_name], 'context': context})
-            if data:
-                total_qty += data[0].get(field_name, 0)
-        return total_qty
-
-    def create_sale_order(self, order_vals):
-        return self.models.execute_kw(self.db, self.uid, self.password, 'sale.order', 'create', [order_vals])
-
-    def update_sale_order(self, order_id, order_vals):
-        return self.models.execute_kw(self.db, self.uid, self.password, 'sale.order', 'write', [[order_id], order_vals])
-
-    def post_message(self, order_id, message):
-        return self.models.execute_kw(self.db, self.uid, self.password, 'sale.order', 'message_post', [order_id], {'body': message})import xmlrpc.client
-import ssl
-
-class OdooClient:
-    def __init__(self, url, db, username, password):
-        self.url = url
-        self.db = db
-        self.username = username
-        self.password = password
-        self.context = ssl._create_unverified_context()
-        
-        self.common = xmlrpc.client.ServerProxy(f'{self.url}/xmlrpc/2/common', context=self.context)
-        self.uid = self.common.authenticate(self.db, self.username, self.password, {})
-        self.models = xmlrpc.client.ServerProxy(f'{self.url}/xmlrpc/2/object', context=self.context)
-
-    def search_partner_by_email(self, email):
-        ids = self.models.execute_kw(self.db, self.uid, self.password,
-            'res.partner', 'search', [[['email', '=', email]]])
-        if ids:
-            partners = self.models.execute_kw(self.db, self.uid, self.password,
-                'res.partner', 'read', [ids], {'fields': ['id', 'name', 'parent_id']})
-            return partners[0]
-        return None
-
-    def search_product_by_sku(self, sku, company_id=None):
-        """Finds product ID by SKU, filtering by Company if provided (Fixed Syntax)"""
-        if company_id:
-            # POLISH NOTATION FIX: ['&', A, '|', B, C]  ==  A AND (B OR C)
-            domain = [
                 '&', 
-                ['default_code', '=', sku], 
-                '|', 
-                ['company_id', '=', int(company_id)], 
-                ['company_id', '=', False]
-            ]
-        else:
-            domain = [['default_code', '=', sku]]
-
-        ids = self.models.execute_kw(self.db, self.uid, self.password,
-            'product.product', 'search', [domain])
-        return ids[0] if ids else None
-
-    def search_product_by_name(self, name, company_id=None):
-        """Finds product ID by Name, filtering by Company if provided (Fixed Syntax)"""
-        if company_id:
-            domain = [
-                '&', 
-                ['name', 'ilike', name], 
-                '|', 
-                ['company_id', '=', int(company_id)], 
-                ['company_id', '=', False]
-            ]
-        else:
-            domain = [['name', 'ilike', name]]
-
-        ids = self.models.execute_kw(self.db, self.uid, self.password,
-            'product.product', 'search', [domain])
-        return ids[0] if ids else None
-
-    def create_service_product(self, name, company_id=None):
-        """Creates a new Service product for shipping charges"""
-        vals = {
-            'name': name,
-            'type': 'service',  # Service type so it doesn't track stock
-            'invoice_policy': 'order',
-            'list_price': 0.0,  # Price determined by order line
-            'sale_ok': True,
-            'purchase_ok': False,
-        }
-        if company_id:
-            vals['company_id'] = int(company_id)
-        
-        return self.models.execute_kw(self.db, self.uid, self.password, 
-            'product.product', 'create', [vals])
-
-    def get_changed_products(self, time_limit_str, company_id=None):
-        """Finds IDs of products changed recently, filtered by Company."""
-        if company_id:
-            # Get products modified recently AND (belong to Company OR Shared)
-            domain = [
-                '&',
-                '&', # Nested AND for the write_date/type
                 ['write_date', '>', time_limit_str], 
                 ['type', '=', 'product'],
                 '|',
