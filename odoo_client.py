9b10ffef30bb3e07b410177e33fe587c9973c1e2
pratzs <pratzs@live.com>
2025-12-08 15:16:34 +1300

    Update odoo_client.py

--- odoo_client.py
+++ odoo_client.py
@@ -7,6 +7,7 @@ class OdooClient:
         self.db = db
         self.username = username
         self.password = password
+        # Fix for some free hosting SSL contexts
         self.context = ssl._create_unverified_context()
         
         self.common = xmlrpc.client.ServerProxy(f'{self.url}/xmlrpc/2/common', context=self.context)
@@ -14,6 +15,7 @@ class OdooClient:
         self.models = xmlrpc.client.ServerProxy(f'{self.url}/xmlrpc/2/object', context=self.context)
 
     def search_partner_by_email(self, email):
+        """Finds a customer and checks if they have a Parent Company"""
         ids = self.models.execute_kw(self.db, self.uid, self.password,
             'res.partner', 'search', [[['email', '=', email]]])
         if ids:
@@ -23,22 +25,26 @@ class OdooClient:
         return None
 
     def search_product_by_sku(self, sku):
+        """Finds product ID by Internal Reference"""
         ids = self.models.execute_kw(self.db, self.uid, self.password,
             'product.product', 'search', [[['default_code', '=', sku]]])
         return ids[0] if ids else None
 
     def search_product_by_name(self, name):
+        """Finds product ID by Name (Useful for Shipping Methods)"""
+        # Case insensitive search ('ilike')
         ids = self.models.execute_kw(self.db, self.uid, self.password,
             'product.product', 'search', [[['name', 'ilike', name]]])
         return ids[0] if ids else None
 
     def get_changed_products(self, time_limit_str):
+        """Finds IDs of products changed recently."""
         domain = [('write_date', '>', time_limit_str), ('type', '=', 'product')]
         return self.models.execute_kw(self.db, self.uid, self.password,
             'product.product', 'search', [domain])
 
     def get_companies(self):
-        """Fetches list of all companies"""
+        """Fetches list of all companies available in Odoo"""
         return self.models.execute_kw(self.db, self.uid, self.password,
             'res.company', 'search_read', [[]], 
             {'fields': ['id', 'name']})
@@ -51,6 +57,7 @@ class OdooClient:
         if not company_id:
             return [] # Return empty if no company selected to save memory
 
+        # Filter for Internal Usage AND specific Company
         domain = [['usage', '=', 'internal'], ['company_id', '=', int(company_id)]]
         
         return self.models.execute_kw(self.db, self.uid, self.password,
@@ -58,8 +65,13 @@ class OdooClient:
             {'fields': ['id', 'complete_name', 'company_id']})
 
     def get_total_qty_for_locations(self, product_id, location_ids, field_name='qty_available'):
+        """
+        Calculates total stock. 
+        field_name can be 'qty_available' (On Hand) or 'virtual_available' (Forecasted/Free)
+        """
         total_qty = 0
         for loc_id in location_ids:
+            # We use the 'context' dictionary to tell Odoo "Read stock from THIS specific location"
             context = {'location': loc_id}
             data = self.models.execute_kw(self.db, self.uid, self.password,
                 'product.product', 'read', [product_id],
@@ -69,10 +81,13 @@ class OdooClient:
         return total_qty
 
     def create_sale_order(self, order_vals):
+        """Creates the order in Odoo"""
         return self.models.execute_kw(self.db, self.uid, self.password, 'sale.order', 'create', [order_vals])
 
     def update_sale_order(self, order_id, order_vals):
+        """Updates an existing order"""
         return self.models.execute_kw(self.db, self.uid, self.password, 'sale.order', 'write', [[order_id], order_vals])
 
     def post_message(self, order_id, message):
+        """Adds a note to the order chatter"""
         return self.models.execute_kw(self.db, self.uid, self.password, 'sale.order', 'message_post', [order_id], {'body': message})
